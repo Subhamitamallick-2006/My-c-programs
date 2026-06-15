8922771e34e1aedd4a69bddf7d15c0ba18c83037
Subhamitamallick-2006 <subhamitamallick20@gmail.com>
2026-06-15 23:09:06 +0530

    subhamita

---
+++ practise/reverse-num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,rev=0;
+
+    printf("enter a number:");
+    scanf("%d",&n);
+
+    while (n>0)
+
+    {
+    rev=rev*10+n%10; 
+    n=n/10;
+    }
+    printf("reverse num =%d",rev);
+
+    return 0;
+
+    
+}
