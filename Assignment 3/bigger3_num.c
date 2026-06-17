ab2ba902fc784247d5e409e461619036fa60493f
Subhamitamallick-2006 <subhamitamallick20@gmail.com>
2026-06-17 18:42:47 +0530

    subhamita

---
+++ Assignment 3/bigger3_num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter 3 numers :\n");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>=b&& a>=c)
+    printf("largest number=%d",a);
+    else if(b>=a&&b>=c)
+    printf("largest number=%d",b);
+    else
+     printf("largest number=%d",c);
+
+    return 0;
+}
