53d17a9477607fcf9b7e083db3a95cee6c3bc7a9
Subhamitamallick-2006 <subhamitamallick20@gmail.com>
2026-06-18 11:52:40 +0530

    subhamita

---
+++ practise/stone_paper_scissors.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>   
+
+
+int main() {
+    int user_choice, computer_choice;
+
+    srand(time(0));
+
+    printf("=== Stone, Paper, Scissors Game ===\n");
+    printf("1. Stone\n2. Paper\n3. Scissors\n");
+    printf("Enter your choice (1, 2, or 3): ");
+    scanf("%d", &user_choice);
+
+    computer_choice = (rand() % 3) + 1;
+
+    if (computer_choice == 1) {
+        printf("Computer chose: Stone\n");
+    } else if (computer_choice == 2) {
+        printf("Computer chose: Paper\n");
+    } else {
+        printf("Computer chose: Scissors\n");
+    }
+
+    if (user_choice == computer_choice) {
+        printf("Result: It's a Tie! 🤝\n");
+    } 
+
+    else if ((user_choice == 1 && computer_choice == 3) || // Stone beats Scissors
+             (user_choice == 2 && computer_choice == 1) || // Paper beats Stone
+             (user_choice == 3 && computer_choice == 2))   // Scissors beats Paper
+    {
+        printf("Result: You Win! 🎉\n");
+    } 
+
+    else {
+        printf("Result: Computer Wins! 🤖\n");
+    }
+
+    return 0;
+}
