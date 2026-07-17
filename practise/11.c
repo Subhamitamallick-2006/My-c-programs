251b516031df094c38c7c1284e1ac576227435f5
Subhamitamallick-2006 <subhamitamallick20@gmail.com>
2026-07-17 23:14:15 +0530

    subhamita

---
+++ practise/11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    float number;
+    printf(" Enter any number :");
+    scanf("%f",&number);
+
+    if (number > 0)
+    printf("Number entered is positive\n");
+    else
+    printf("Number entered is negative\n");
+
+
+}
---
+++ practise/12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    float number;
+    printf(" Enter any number :");
+    scanf("%f",&number);
+
+    if (number > 0)
+    printf("Number entered is positive\n");
+
+
+}
